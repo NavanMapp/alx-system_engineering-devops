39e4a18db1a291baa9ad00ff55bdfa5ad8cfde6d
NavanMapp <navanmapp@gmail.com>
2023-05-11 16:50:28 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,25 @@
 """A function that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit"""
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers from the Reddit API"""
-    r = requests.get(r'https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    if r.status_code != 200:
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    json = r.json()
-    data = json.get('data')
-    return data.get('subscribers', 0)
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
