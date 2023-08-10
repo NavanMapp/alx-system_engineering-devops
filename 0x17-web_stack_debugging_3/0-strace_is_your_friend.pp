5423289b8b7bd82c1fbb9b74f1b186b97aff38f2
NavanMapp <navanmapp@gmail.com>
2023-08-10 14:48:14 +0000

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,12 +5,13 @@
 
 # Define an Exec resource to fix the issue (replace with actual commands)
 file { '/path/to/apache/error.log':
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['apache2'], # Ensure Apache is installed before applying this change
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0644',
+require => Package['apache2'], # Ensure Apache is installed before applying this change
 }
+service {
+'apache2':
+ensure => 'running',
 
-service { 'apache2':
-  ensure => 'running',
 }
